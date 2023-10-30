ff8f0133a316ae3a2e20d0e93ca5563c50217ac6
adriancarranza <adriancarranza0113@gmail.com>
2023-10-30 11:58:51 -0700

    treasure bomb complete

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Square from "./components/Square"
 
 import "./App.css"
 import { upload } from "@testing-library/user-event/dist/upload"
+import { click } from "@testing-library/user-event/dist/click"
 
 const App = () => {
   const [board, setBoard] = useState([
@@ -18,17 +19,32 @@ const App = () => {
     "?",
   ])
 
+const [treasureLocation, setTreasureLocation] = useState(Math.floor(Math.random()* board.length))
+
+const [bombLocation, setBombLocation] = useState(
+  Math.floor(Math.random() * board.length)
+)
+
 const handleSquareClick = (clickedSquareIndex) => {
   // create a variable holding copy of current state
   let updatedBoard = [...board]
+  // set condition for if treasure locaiton is same as clicked squares's index show a treasure
+  if(clickedSquareIndex === treasureLocation) {
+    // then reassign state value at the index to treasure emoji
+    updatedBoard[clickedSquareIndex] = "💎"
+  } else if (clickedSquareIndex === bombLocation) {
+    updatedBoard[clickedSquareIndex] = "💣"
+  } else {
   // use index from clickedSqureIndex to update the current square's value with emoji using bracket notation
   updatedBoard[clickedSquareIndex] = "🌲"
   // update state with the new board
-  setBoard(updatedBoard)
+ 
+} 
+setBoard(updatedBoard)
 }
 
   return (
-    <>
+    <> 
       <h1>Treasure Hunt Game</h1>
       <div className = "board">
       {/* Map over array and return a square for each element */}
